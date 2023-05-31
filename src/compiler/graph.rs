852b6116a2f00d9f733400d076c4fdc86ad1ea72
Balder W. Holst <balderwh@gmail.com>
2023-05-31 10:44:32 +0200

    Manually constructed graph

--- src/compiler/graph.rs
+++ src/compiler/graph.rs
@@ -1,52 +1,121 @@
 use fnv::FnvHashMap;
 use std::hash::Hash;
+use std::rc::Rc;
 
-struct Graph<VId, E = (), V = ()> {
-    vertices: FnvHashMap<VId, V>,
-    adjacency: FnvHashMap<VId, Vec<(VId, E)>>,
+#[derive(Clone, Debug)]
+pub enum ArithmeticOperation {
+    ADD,
+    SUBTRACT,
+    MULTIPLY,
+    DIVIDE,
 }
 
-impl<VId, E, V> Graph<VId, E, V> 
-where 
-    VId: Eq + Hash, 
-    V: Hash 
+#[derive(Clone, Debug)]
+pub enum IOType {
+    Signal(String),
+    AnySignal(u64),
+    Constant(i32)
+}
+
+#[derive(Clone, Debug)]
+pub struct ArithmeticConnection {
+    left: IOType,
+    right: IOType,
+    operation: ArithmeticOperation,
+    output: IOType,
+}
+
+
+impl ArithmeticConnection {
+    pub fn new(left: IOType, right: IOType, operation: ArithmeticOperation, output: IOType) -> Self {
+        Self {left, right, operation, output}
+    }
+
+    pub fn new_pick(t: IOType) -> Self {
+        Self::new(t.clone(), IOType::Constant(0), ArithmeticOperation::ADD, t)
+    }
+}
+
+#[derive(Clone, Debug)]
+pub enum Connection {
+    Arithmetic(ArithmeticConnection)
+}
+
+
+#[derive(Clone, Debug)]
+pub struct Node {
+    inputs: Vec<Edge>,
+}
+
+impl Node {
+    pub fn new() -> Self {
+        Self { inputs: vec![] }
+    }
+
+    fn add_input(&mut self, edge: Edge) {
+        self.inputs.push(edge);
+    }
+}
+
+pub type Edge = Rc<Connection>;
+type VId = u64;
+
+pub struct Graph {
+    vertices: FnvHashMap<VId, Node>,
+    adjacency: FnvHashMap<VId, Vec<(VId, Edge)>>,
+    next_vid: VId,
+}
+
+impl Graph 
 {
-    pub fn new() -> Graph<VId, E, V> {
-        Graph { vertices: FnvHashMap::default(), adjacency: FnvHashMap::default() }
+    pub fn new() -> Graph {
+        Graph { vertices: FnvHashMap::default(), adjacency: FnvHashMap::default(), next_vid: 0 }
     }
 
-    pub fn push_vertex(&mut self, vid: VId, vertex: V) {
-        self.vertices.insert(vid, vertex);
+    pub fn get_vertex(&self, vid: &VId) -> Option<&Node> {
+        self.vertices.get(vid)
     }
 
-    pub fn push_edge(&mut self, from: VId, to: VId, edge: E) {
+    pub fn get_mut_vertex(&mut self, vid: &VId) -> Option<&mut Node> {
+        self.vertices.get_mut(vid)
+    }
+
+    pub fn push_vertex(&mut self, vertex: Node) -> VId {
+        let vid = self.next_vid;
+        if self.vertices.insert(vid, vertex).is_some() {
+            panic!("Could not insert node into graph")
+        }
+        self.next_vid += 1;
+        vid
+    }
+
+    pub fn push_edge(&mut self, from: VId, to: VId, edge: Edge) {
+        self.get_mut_vertex(&to).expect(&format!("Could not access vertex: {}", to)).add_input(edge.clone());
         let adjacent_to_from = self.adjacency.entry(from).or_default();
-        adjacent_to_from.push((to, edge))
+        adjacent_to_from.push((to, edge));
     }
-}
 
-impl<VId, E, V> Graph<VId, E, V>
-where
-    VId: Eq + Hash + Clone,
-    V: Hash,
-    E: Clone,
-{
     pub fn push_undirected_edge(
         &mut self,
         from: VId,
         to: VId,
-        edge: E,
+        edge: Edge,
         ) {
         self.push_edge(from.clone(), to.clone(), edge.clone());
         self.push_edge(from, to, edge);
     }
-}
 
-impl<VId, E> Graph<VId, E, ()>
-where
-    VId: Eq + Hash,
-{
-    pub fn push_vid(&mut self, vid: VId) {
-        self.vertices.insert(vid, ());
+    pub fn print(&self) {
+        println!("Graph:");
+        println!("\tVertecies:");
+        for (k, v) in &self.vertices {
+            println!("\t\t{} {:?}", k, v)
+        }
+        println!("\n\tConnections:");
+        for (vid, to) in &self.adjacency {
+            for (k, v) in to {
+                println!("\t\t{} -> {} : {:?}", vid, k, v);
+            }
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use crate::ast::lexer::{Lexer, Token};
 use crate::ast::parser::Parser;
 use crate::ast::AST;
+use crate::compiler::graph::IOType;
+
+use compiler::graph::*;
 
 mod ast;
+mod compiler;
 
 fn main() {
     let lexer = Lexer::from_file("./examples/simple_block.rcp").unwrap();
@@ -19,4 +23,49 @@ fn main() {
 
     println!("\nPrinting AST:");
     ast.print();
+
+    let mut graph = Graph::new();
+    let input = graph.push_vertex(Node::new());
+    let v2 = graph.push_vertex(Node::new());
+    let v3 = graph.push_vertex(Node::new());
+    let output = graph.push_vertex(Node::new());
+
+    graph.push_edge(input, v2, Edge::new(
+            Connection::Arithmetic(
+                ArithmeticConnection::new(
+                    IOType::Signal("BLUE".to_string()),
+                    IOType::Signal("GREEN".to_string()),
+                    ArithmeticOperation::MULTIPLY,
+                    IOType::AnySignal(0),
+                    )
+                )));
+
+    graph.push_edge(input, v2, Edge::new(
+            Connection::Arithmetic(
+                ArithmeticConnection::new_pick(IOType::Signal("RED".to_string()))
+                )));
+
+    graph.push_edge(v2, v3, Edge::new(
+            Connection::Arithmetic(
+                ArithmeticConnection::new(
+                    IOType::Signal("RED".to_string()), 
+                    IOType::AnySignal(0), 
+                    ArithmeticOperation::ADD,
+                    IOType::AnySignal(1),
+                    )
+                )));
+
+    graph.push_edge(v3, output, Edge::new(
+            Connection::Arithmetic(
+                ArithmeticConnection::new(
+                    IOType::AnySignal(1),
+                    IOType::Constant(500),
+                    ArithmeticOperation::SUBTRACT,
+                    IOType::Signal("WHITE".to_string()),
+                    )
+                )));
+
+    graph.print();
+
+    println!("DONE.");
 }
