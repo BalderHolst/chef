54f51d972af964f0e12a3aecaefab9225229e192
Balder W. Holst <balderwh@gmail.com>
2023-06-06 16:33:05 +0200

    Evaluate constant expressions

--- src/compiler/graph_optimizer.rs
+++ src/compiler/graph_optimizer.rs
@@ -46,12 +46,58 @@ impl<'a> GraphOptimizer<'a> {
             if let IOType::Constant(_) = inputs.first().unwrap() { }
             else { return; }
         }
+        else if inputs.len() == 0 {
+            for (to_vid, conn) in self.graph.adjacency[&vid].clone() {
+                if let Connection::Arithmetic(ac) = conn.as_ref() {
+                    if let IOType::Constant(left_value) = ac.left {
+                        if let IOType::Constant(right_value) = ac.right {
+                            let result = match ac.operation {
+                                super::graph::ArithmeticOperation::ADD => left_value + right_value,
+                                super::graph::ArithmeticOperation::SUBTRACT => left_value - right_value,
+                                super::graph::ArithmeticOperation::MULTIPLY => left_value * right_value,
+                                super::graph::ArithmeticOperation::DIVIDE => left_value / right_value,
+                            };
+                            let out_type = IOType::Constant(result);
+                            let new_input_vid = self.graph.push_input_node("internal (todo)".to_string(), vec![out_type.clone()]);
+                            self.graph.remove_node_with_connections(&vid);
+                            let new_conn = self.graph.push_connection(new_input_vid, to_vid, Connection::Arithmetic(ArithmeticConnection::new_pick(out_type)));
+
+                            // TODO: Virker ikke...
+                            // Remove reference to old connection 
+                            match self.graph.vertices.get_mut(&to_vid) {
+                                Some(Node::Inner(n)) => {
+                                    for (i, c) in n.inputs.iter().enumerate() {
+                                        if c == &conn {
+                                            n.inputs.remove(i);
+                                            break;
+                                        }
+                                    }
+                                    n.inputs.push(new_conn);
+                                },
+                                Some(Node::Output(n)) => {
+                                    for (i, c) in n.inputs.iter().enumerate() {
+                                        if c == &conn {
+                                            n.inputs.remove(i);
+                                            break;
+                                        }
+                                    }
+                                    n.inputs.push(new_conn);
+                                },
+                                Some(Node::Input(_n)) => panic!("This should not be possible."),
+                                None => {},
+                            }
+
+                        }
+                        return;
+                    }
+                }
+            }
+        }
         else { return; };
 
         for (to_vid, connection) in self.graph.adjacency.get(&vid).unwrap().clone() {
             if connection.is_pick() {
                 self.graph.remove_node_with_connections(&vid);
-                // self.graph.remove_connection(vid, to_vid.clone(), &connection);
             }
             self.integrate_constant_input(to_vid);
         }
