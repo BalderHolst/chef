1d4fbb7979bef8e429b81cec8a2f076b2d40d9e6
Balder W. Holst <balderwh@gmail.com>
2023-07-01 12:35:44 +0200

    Seperate text file for advice

--- chef/src/the_chef/mod.rs
+++ chef/src/the_chef/mod.rs
@@ -1,30 +1,10 @@
 use rand::prelude::*;
 
-const ADVICE: &'static [&'static str] = &[
-    "Are you following a recipe?",
-    "Have you sharpened your knives recently? That may help!",
-    "Maybe add some garlic powder?",
-    "I think it needs more salt.",
-    "You could always just order something...",
-    "It needs a little spice.",
-    "Not quite done.",
-    "Overcooked.",
-    "Uff.",
-    "Are you sure those are the right proportions?",
-    "Measure twice, mix once.",
-    "Have you remembered to preheat the oven?",
-    "...is that how its supposed to look?",
-    "Experimentation is good!",
-    "If you mix cream too long, it'll turn into butter!",
-    "A good cook knows their spices!",
-    "That is... unique.",
-    "Did you read the entire recipe?",
-    "Its good to clean as you go.",
-];
+const ADVICE: &str = include_str!("advice.txt");
 
 /// Prints a random line of cooking advice to the terminal
 pub fn give_advice() {
-    let index: usize = rand::thread_rng().gen_range(0..ADVICE.len());
-    let advice = ADVICE[index];
+    let index: usize = rand::thread_rng().gen_range(0..ADVICE.lines().count());
+    let advice = ADVICE.lines().nth(index).unwrap();
     println!("{advice}");
 }
