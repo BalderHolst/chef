c28cb18d436bf1a3793999cdb7cc5922e448f7b2
Balder W. Holst <balderwh@gmail.com>
2023-07-18 10:45:35 +0200

    Make failing test nicer to view

--- chef/src/main.rs
+++ chef/src/main.rs
@@ -136,6 +136,7 @@ mod tests {
     fn check_dot_files() {
         let example_dir = "examples";
         let out_dir = "example_outputs".to_string();
+        let graph_out = "test.svg";
         for file in fs::read_dir(example_dir).unwrap() {
             let file = file.unwrap().path();
             println!("\nCompiling: {}... ", file.display());
@@ -148,7 +149,7 @@ mod tests {
             let text = Rc::new(SourceText::from_file(file.to_str().unwrap()).unwrap());
             let opts = Rc::new(Opts::default());
             let bag = DiagnosticsBag::new_ref(opts.clone(), text.clone());
-            let ast = AST::from_source(text, bag.clone(), opts);
+            let ast = AST::from_source(text.clone(), bag.clone(), opts);
             bag.borrow_mut().exit_if_errored();
             let graph = compiler::compile(ast, bag.clone()).unwrap();
             bag.borrow_mut().exit_if_errored();
@@ -158,10 +159,16 @@ mod tests {
             if expected_dot != compiled_dot {
                 let diff = prettydiff::diff_chars(&expected_dot, &compiled_dot)
                     .set_highlight_whitespace(true);
-                println!("EXPECTED:\n{expected_dot}\n");
-                println!("COMPILED:{compiled_dot}\n");
-                println!("DIFF:\n{diff}\n");
-                panic!("Compiled dot is different from expected.")
+                let _ = graph.visualize(graph_out);
+                cli::print_label("CODE");
+                println!("{}\n", text.text());
+                println!("Graph saved to {}\n", graph_out);
+                cli::print_label("DIFF");
+                println!("{diff}\n");
+                panic!(
+                    "Compiled dot is different from expected in \"{}\".",
+                    file.display()
+                )
             }
         }
     }
