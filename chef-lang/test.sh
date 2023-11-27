9701a241730f1ea815efae515b5f595391cbd8fd
Balder W. Holst <balderwh@gmail.com>
2023-11-27 15:02:32 +0100

    update `test.sh` script

--- chef-lang/test.sh
+++ chef-lang/test.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-cargo run -- cook $1 -g tmp.svg -G tmp2.svg && bat $1 && {
-    eog tmp.svg & eog tmp2.svg
-}
+cargo run -- cook -v $1 -g tmp.svg -G tmp2.svg && bat $1 && eog tmp.svg
